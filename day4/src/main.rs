5191a15ff6e500548c92640421e668df81f4c6cd
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2024-12-05 15:13:34 +0000

    add day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -18,29 +18,23 @@ fn main() {
     .map(|line| line.chars().collect())
     .collect();
 
-  let diri = [-1, 0, 1, 0, -1, 1, -1, 1] as [i32; 8];
-  let dirj = [0, 1, 0, -1, -1, 1, 1, -1] as [i32; 8];
-  let need = ['X', 'M', 'A', 'S'];
   let mut ans = 0;
 
-  for i in 0..char_matrix.len() {
-    for j in 0..char_matrix[i].len() {
-      if char_matrix[i][j] == 'X' {
-        for dir in 0..8 {
-          let mut flag = true;
-          for step in 0..4 {
-            let ni = (i as i32) + diri[dir] * step;
-            let nj = (j as i32) + dirj[dir] * step;
-            if !((ni >= 0 && ni < char_matrix.len() as i32 && nj >= 0 && nj < char_matrix[ni as usize].len() as i32)) || char_matrix[ni as usize][nj as usize] != need[step as usize] {
-              flag = false;
-              break;
-            }
-          }
-          if flag {
-            println!("Found at ({}, {}, {})", i, j, dir);
-          }
-          ans += flag as i32;
-        } 
+  for i in 1..char_matrix.len()-1 {
+    for j in 1..char_matrix[i].len()-1 {
+      if char_matrix[i][j] == 'A' {
+        if char_matrix[i-1][j-1] == 'M' && char_matrix[i-1][j+1] == 'M' && char_matrix[i+1][j-1] == 'S' && char_matrix[i+1][j+1] == 'S' {
+          ans += 1;
+        }
+        if char_matrix[i-1][j-1] == 'S' && char_matrix[i-1][j+1] == 'S' && char_matrix[i+1][j-1] == 'M' && char_matrix[i+1][j+1] == 'M' {
+          ans += 1;
+        }
+        if char_matrix[i-1][j-1] == 'S' && char_matrix[i-1][j+1] == 'M' && char_matrix[i+1][j-1] == 'S' && char_matrix[i+1][j+1] == 'M' {
+          ans += 1;
+        }
+        if char_matrix[i-1][j-1] == 'M' && char_matrix[i-1][j+1] == 'S' && char_matrix[i+1][j-1] == 'M' && char_matrix[i+1][j+1] == 'S' {
+          ans += 1;
+        }
       }
     }
   }
