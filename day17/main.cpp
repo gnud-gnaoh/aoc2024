6521a5eaa19943e52cb77dd0f689c7692ce3d3b7
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2024-12-17 11:26:18 +0000

    add day17 part2

--- day17/main.cpp
+++ day17/main.cpp
@@ -7,13 +7,14 @@ signed main() {
   freopen("output", "w", stdout);
   freopen("error", "w", stderr);
 
-  int a, b, c;
-  cin >> a >> b >> c;
+  int a = 0;
+  int b = 0;
+  int c = 0;
 
   vector<int> ops;
   int tmp;
   while (cin >> tmp) ops.push_back(tmp);
-
+  
   auto combo = [&](int x) {
     if (x <= 3) return x;
     if (x == 4) return a;
@@ -23,39 +24,79 @@ signed main() {
     return x;
   };
 
-  int p = 0;
-  while (p < ops.size()) {
-    int ins = ops[p++];
-    if (p == ops.size()) break;
-    if (ins == 3 && a == 0) continue;
+  auto f = [&](int aa) {
+    a = aa;
+    b = 0;
+    c = 0;
 
-    int op = ops[p++];
+    vector<int> res;
+    int p = 0;
+    bool ok = true;
+    while (p < ops.size()) {
+      int ins = ops[p++];
+      if (p == ops.size()) break;
 
-    if (ins == 0) {
-      op = combo(op);
-      a = a >> op;
-    } else if (ins == 1) {
-      b = b ^ op;
-    } else if (ins == 2) {
-      op = combo(op);
-      b = op % 8;
-    } else if (ins == 3) {
-      if (a == 0) continue;
-      p = op;
-    } else if (ins == 4) {
-      b = b ^ c;
-    } else if (ins == 5) {
-      cout << (combo(op) % 8) << ',';
-    } else if (ins == 6) {
-      op = combo(op);
-      b = a >> op;
-    } else if (ins == 7) {
-      op = combo(op);
-      c = a >> op;
-    } else {
-      assert(false);
+      int op = ops[p++];
+
+      if (ins == 0) {
+        op = combo(op);
+        a = a >> op;
+      } else if (ins == 1) {
+        b = b ^ op;
+      } else if (ins == 2) {
+        op = combo(op);
+        b = op % 8;
+      } else if (ins == 3) {
+        if (a == 0) continue;
+        p = op;
+      } else if (ins == 4) {
+        b = b ^ c;
+      } else if (ins == 5) {
+        res.push_back(combo(op) % 8);
+        // if (res.back() != ops[res.size() - 1]) {
+        //   ok = false;
+        //   break;
+        // }
+      } else if (ins == 6) {
+        op = combo(op);
+        b = a >> op;
+      } else if (ins == 7) {
+        op = combo(op);
+        c = a >> op;
+      } else {
+        assert(false);
+      }
     }
-  }
+    return res;
+  };
 
-  cerr << a << " " << b << " " << c << '\n';
+  // for (int aa = 0; aa < 10000; aa++) {
+  //   auto x = f(aa);
+  //   for (int y : x) cout << y << " ";
+  //   cout << '\n';
+  // }
+  // return 0;
+
+  vector<int> v = ops;
+
+  auto rec = [&](auto self, int ans, int i) -> void {
+    if (i < 0) {
+      cout << ans;
+      exit(0);
+    }
+    if (f(ans)[i] == v[i]) {
+      self(self, ans, i - 1);
+    }
+    int len = i + 1;
+    int l = 1LL << (3 * (len - 1));
+    int r = 1LL << (3 * len);
+
+    for (int ll = l; ll < r; ll += l) {
+      if (f(ans + ll)[i] == v[i]) {
+        self(self, ans + ll, i - 1);
+      }
+    }
+  };
+
+  rec(rec, 0, v.size() - 1);
 }
